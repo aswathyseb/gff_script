3f30f7c99b5243d95e9b2883cc364afae7006050
Natay Aberra <natay.aberra@gmail.com>
2020-02-07 18:11:04 -0500

    fix up

--- merge/gff.py
+++ merge/gff.py
@@ -1,134 +1,171 @@
 import csv
 
 
+TEST_AUGUSTUS = "aug.gff3"
+TEST_STRINGTIE = "stringtie.gff3"
+
+
 def parse_row(row):
     # chrom, start, end, strand, attr, feat
     return row[0], row[3], row[4], row[6], row[8], row[2]
 
 
-def check_overlap(gmeta_data, tmeta_data):
-    return
-
-
-def extract_attrs(attr_str, attr_name):
-    # Find star and ending index
+def parse_attrs(attr_str, attr_name):
+    # Find start and ending index of the attribute we want
+    if not attr_str:
+        return ""
     start_idx = attr_str.find(attr_name)
-
     end_idx = attr_str[start_idx:].find(";")
 
     # Slice string to the attribute we want
     value = attr_str[start_idx:end_idx]
-
     value = value.replace(f"{attr_name}=", "")
 
     return value
 
 
-def is_overlap(chrom1="", chrom2="",end1=0, start1=0,end2=0, start2=0):
-    if chrom1 != chrom2:
+def parse_file(gff, target="gene"):
+    ann_store = dict()
+    meta_data = dict()
+    stream = csv.reader(open(gff), delimiter="\t")
+    key = None
+    for row in stream:
+        if row.startswith("#"):
+            continue
+
+        chrom, start, end, strand, attr, feat = parse_row(row)
+        if feat == target:
+            key = parse_attrs(attr, "ID")
+            # Fill meta data one for each key
+            meta_data[key] = dict(chrom=chrom, start=start, end=end, attr=attr, strand=strand)
+        if key:
+            ann_store.setdefault(key, []).append(row)
+
+    return ann_store, meta_data
+
+
+def calculate_coverage(tmeta, gmeta):
+
+    ch1, ch2 = tmeta['chrom'], gmeta['chrom']
+    en1, en2 = tmeta['end'], gmeta['end']
+    st1, st2 = tmeta['start'], gmeta['start']
+
+    cov = 0
+    len1 = abs(en1 - st1) + 1
+    len2 = abs(en2 - st2) + 1
+
+    within = st1 >= st2 and en1 <= en2
+    within2 = st1 <= st2 and en1 >= en2
+    # Parameters to evaluate left and right transcript coverage.
+    left = st1 < st2 and en1 >= st2 and en1 < en2
+    right = st1 > st2 and st2 < en2 and en1 > en2
+
+    if within:
+        cov = ((en1 - st1) / len1) * 100
+    if within2:
+        cov = ((en2 - st2) / len2) * 100
+    if left:
+        cov = ((en1 - st2) / len1) * 100
+    if right:
+        cov = ((en2 - st1) / len1) * 100
+
+    return cov, ch1, ch2
+
+
+def is_overlap(tmeta, gmeta):
+    # Parse relevant coordinates from meta data
+    coverage, ch1, ch2 = calculate_coverage(tmeta=tmeta, gmeta=gmeta)
+
+    if ch1 != ch2:
         return False
         # if strand1 != strand2:
         #    return False
 
-    cov = 0
-    len1 = abs(end1 - start1) + 1
-    len2 = abs(end2 - start2) + 1
-
-    overlap_within = start1 >= start2 and end1 <= end2
-    overlap_within2 = start1 <= start2 and end1 >= end2
-
-    # check overlap - within a transcript
-    if overlap_within:
-        cov = ((end1 - start1) / len1) * 100
-
-    if overlap_within2:
-        cov = ((end2 - start2) / len2) * 100
-
-    if start1 < start2 and (end1 >= start2 and end1 < end2):
-        # overlap left
-        cov = ((end1 - start2) / len1) * 100
-    if start1 > start2 and (start1 < end2 and end1 > end2):
-        # overlap right
-        cov = ((end2 - start1) / len1) * 100
-    if chrom1 == chrom2 and cov >= 60:
+    if ch1 == ch2 and coverage >= 60:
         return True
 
     return False
 
 
-def parse_cords(mdata1, mdata2):
-    chrom1 = mdata1['chrom']
-    chrom2 = mdata2['chrom']
-    end1 = mdata1['end']
-    start1 = mdata1['start']
-    end2 = mdata2['end']
-    start2 = mdata2['start']
-
-    return chrom1, chrom2, end1, end2, start1, start2
+def check_overlap(gmeta, tmeta):
+    overlap_store = []
+    for gene, gene_meta in gmeta.items():
+        # Append gene to list of overlapping genes
+        if is_overlap(tmeta=tmeta, gmeta=gmeta):
+            overlap_store.append(gene)
+    return overlap_store
 
 
-def create_merged_gff(gstore, gmeta_data, tstore, tmeta_data):
+def get_count(rows, target_str, target_idx):
+
+    # Filter for rows that have the target_str in the index ( target_idx )
+    target_lst = list(filter(lambda l: l[target_idx] == target_str, rows))
+    count = len(target_lst)
+    return count
+
+
+def handle_gene_overlap(trans_meta, gmeta_data, gstore, count, overlap_store, prev_gene=""):
+    # Get the transcript counts for this gene
+    gene_overlap = len(overlap_store) == 1
+    g_key = overlap_store[0] if gene_overlap else ""
+    gene_meta = gmeta_data.get(g_key, {})
+    t_best = parse_attrs(trans_meta['attr'], "best_match")
+    g_best = parse_attrs(gene_meta.get('attr', ""), "best_match")
+
+    g_best = parse_attrs(gene_meta.get('attr', ""), "best_match")
+
+    if t_best != "stringtie" and g_best != "augustus":
+        return
+
+
+    return
+
+
+def create_merged_gff(gene_file, transcript_file):
+    gstore, gmeta_data = parse_file(gene_file, "gene")
+    tstore, tmeta_data = parse_file(transcript_file, "transcript")
+    prev_gene, count = "", 0
 
     for tkey, tvals in tstore.items():
 
         # Get the transcript meta data
         trans_meta = tmeta_data.get(tkey)
+        trans_strand = trans_meta['strand']
 
         # Keep track of genes overlapping with this transcript.
-        overlap_store = []
+        overlap_store = check_overlap(gmeta_data, trans_meta)
+        is_overlapped = len(overlap_store) > 1
+        is_gene_overlap = len(overlap_store) == 1
+        g_key = overlap_store[0] if is_gene_overlap else ""
+        gene_meta = gmeta_data.get(g_key, {})
+        gene_strand = gene_meta.get('strand', '')
+        gene = gstore[g_key]
+        # CASE 1 : # transcript overlaps with more than one gene - remove transcript.
+        if is_overlapped:
+            continue
 
-        for gene, gene_meta in gmeta_data.items():
-            # Parse relevant coordinates from meta data
-            ch1, ch2, en1, en2, st1, st2 = parse_cords(mdata1=trans_meta, mdata2=gene_meta)
-            # Check for overlap
-            if is_overlap(chrom1=ch1, chrom2=ch2, end1=en1, start1=st1, end2=en2, start2=st2):
-                # Append gene to list of overlapping genes
-                overlap_store.append(gene)
+        # Discard if the overlapped gene and transcript do not have the same orientation
+        if is_gene_overlap and gene_strand != trans_strand:
+            continue
 
-            # CASE 1 : # transcript overlaps with more than one gene - remove transcript.
-            if len(overlap_store) > 1:
-                continue
+        if is_gene_overlap:
 
-        print(chrom, attr)
+            tcount = get_count(rows=gene, target_str='transcript', target_idx=2)
+            count = count + 1 if g_key == prev_gene else 1
+            tcount += count
+            prev_gene = g_key
+
+            handle_gene_overlap(prev_gene=prev_gene)
+
         1 / 0
         pass
 
     return
 
 
-def parse_file(gff, target_feat="gene"):
-    ann_store = dict()
-    meta_data = dict()
-    stream = csv.reader(open(gff), delimiter="\t")
-    feat_id = None
-
-    for row in stream:
-
-        chrom, start, end, strand, attr, feat = parse_row(row)
-
-        if chrom.startswith("#"):
-            continue
-
-        if feat == target_feat:
-            feat_id = extract_attrs(attr, "ID")
-            mdata = dict(chrom=chrom, start=start, end=end, attr=attr, strand=strand)
-            meta_data[feat_id] = mdata
-
-        ann_store.setdefault(feat_id, []).append(row)
-
-    return ann_store, meta_data
-
-
 def main():
-    aug_gff = "test_aug.gff3"
-    aug_gff_store = parse_file(aug_gff, "gene")
 
-    #
-    # print(len(aug_gff_store))
-    # for k, val in aug_gff_store.items():
-    #     for v in val:
-    #         v = list(map(str, v))
-    #         print("\t".join(v))
+    create_merged_gff(gene_file=TEST_AUGUSTUS, transcript_file=TEST_STRINGTIE)
 
     return
 
